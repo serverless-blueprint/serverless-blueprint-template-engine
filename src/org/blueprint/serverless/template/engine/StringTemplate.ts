fd383a8bb3ab638fcd16dceb4c2124704835e7e5
sarthakmakhija <sarthak.makhija@gmail.com>
2019-11-24 15:17:04 +0530

    Sarthak | Adds a type alias PlaceholderOptions

--- src/org/blueprint/serverless/template/engine/StringTemplate.ts
+++ src/org/blueprint/serverless/template/engine/StringTemplate.ts
@@ -1,10 +1,12 @@
 import * as Mustache from 'Mustache';
 
+type PlaceholderOptions = { [key: string]: any }
+
 export class StringTemplate {
     constructor(private template: string) {
     }
 
-    mergeWith(options: {}): string {
+    mergeWith(options: PlaceholderOptions): string {
         return Mustache.render(this.template, options);
     }
 }
